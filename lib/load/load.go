8a99cd4416caf10e3833f316a9915629f46c83a9
Dumb Thing <user@hannah.local>
2015-02-05 08:44:23 +0100

    fix

--- lib/load/load.go
+++ lib/load/load.go
@@ -10,21 +10,24 @@ import (
 )
 
 func Init(minsec int, maxsec int, duration int, errLog *log.Logger) {
+	errLog.Println("load starting")
 	for {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		//We sleep for a range between min and max.
+		errLog.Println("Sleeping.")
 		time.Sleep(time.Duration(r.Intn(maxsec-minsec)+minsec) * time.Second)
-		exhaustForNseconds(time.Duration(duration) * time.Second)
+		exhaustForNseconds(time.Duration(duration)*time.Second, errLog)
 		errLog.Printf("CPU stress cycle for %d seconds finished.\n", duration)
 
 	}
 }
-func exhaustForNseconds(duration time.Duration) {
+func exhaustForNseconds(duration time.Duration, errLog *log.Logger) {
 	s := []byte("load")
 	runtime.GOMAXPROCS(runtime.NumCPU() * 20)
 	quit := make(chan struct{})
-	time.AfterFunc(duration*time.Second, func() { close(quit) })
+	time.AfterFunc(duration, func() { close(quit) })
 	var wg sync.WaitGroup
+	errLog.Println("Spawning", runtime.NumCPU()*20, "worker processes.")
 	for i := 0; i <= runtime.NumCPU()*20; i++ {
 		wg.Add(1)
 		go func() {
@@ -40,5 +43,4 @@ func exhaustForNseconds(duration time.Duration) {
 		}()
 	}
 	wg.Wait()
-
 }
