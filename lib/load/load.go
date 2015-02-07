11b7e6e3b3de6785b594c4acc6ee906e6f733b55
Dumb Thing <user@hannah.local>
2015-02-07 18:15:15 +0100

    removed some debug stuff

--- lib/load/load.go
+++ lib/load/load.go
@@ -14,20 +14,18 @@ func Init(minsec int, maxsec int, duration int, errLog *log.Logger) {
 	for {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		//We sleep for a range between min and max.
-		errLog.Println("Sleeping.")
 		time.Sleep(time.Duration(r.Intn(maxsec-minsec)+minsec) * time.Second)
-		exhaustForNseconds(time.Duration(duration)*time.Second, errLog)
+		exhaustForNseconds(time.Duration(duration) * time.Second)
 		errLog.Printf("CPU stress cycle for %d seconds finished.\n", duration)
-
 	}
 }
-func exhaustForNseconds(duration time.Duration, errLog *log.Logger) {
+
+func exhaustForNseconds(duration time.Duration) {
 	s := []byte("load")
 	runtime.GOMAXPROCS(runtime.NumCPU() * 20)
 	quit := make(chan struct{})
 	time.AfterFunc(duration, func() { close(quit) })
 	var wg sync.WaitGroup
-	errLog.Println("Spawning", runtime.NumCPU()*20, "worker processes.")
 	for i := 0; i <= runtime.NumCPU()*20; i++ {
 		wg.Add(1)
 		go func() {
